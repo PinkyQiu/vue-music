a71296ae63bb53088198f0caee3538c920b306be
v_zpqiu(邱曾评) <v_zpqiu@tencent.com>
2019-11-22 17:55:38 +0800

    部分页面

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import MHeader from 'src/components/m-header/m-header'
+import Tab from 'src/components/tab/tab'
+export default {
+  components: {
+    MHeader,
+    Tab
+  }
+}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import fastclick from 'fastclick'
+import router from './router'
+import 'common/stylus/index.styl'
+import VueLazyLoad from 'vue-lazyload'
+
+fastclick.attach(document.body)
+Vue.config.productionTip = false
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
